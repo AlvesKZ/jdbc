39125f971acf006c2b11f00a88ff4bfebc0e09b4
AlvesKZ <igoralvex01@gmail.com>
2026-02-04 20:13:28 -0300

    feat: update data in database

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,53 +2,37 @@ import db.DB;
 import db.DbExcepition;
 
 import java.sql.*;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 
 
 public class Main {
 
     public static void main(String[] args) {
 
-        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-        Connection conn = null;
-        PreparedStatement st = null;
-        try {
-            conn = DB.getConnection();
+       Connection conn = null;
+       PreparedStatement st = null;
+       try {
+           conn = DB.getConnection();
 
-            st = conn.prepareStatement(
-                    "INSERT INTO seller "
-                    + "(Name, Email, BirthDate, BaseSalary, DepartmentId)"
-                    + "VALUES "
-                    + "(?, ?, ?, ?, ?)",
-                    Statement.RETURN_GENERATED_KEYS
-            );
+           st = conn.prepareStatement(
+                   "UPDATE seller "
+                   + "SET BaseSalary = BaseSalary + ? "
+                   + "WHERE "
+                   + "(DepartmentId = ?)"
+           );
 
-            st.setString(1, "Carl");
-            st.setString(2, "test@email.com");
-            st.setDate(3, new java.sql.Date(sdf.parse("01/05/2026").getTime()));
-            st.setDouble(4, 3000.0);
-            st.setInt(5, 4);
+           st.setDouble(1, 200.0);
+           st.setInt(2, 2);
 
-            int rowsAffected = st.executeUpdate();
+           int rowsAffected = st.executeUpdate();
 
-            if (rowsAffected > 0) {
-                ResultSet rs = st.getGeneratedKeys();
-                while (rs.next()) {
-                    int id = rs.getInt(1);
-                    System.out.println("Done! id = " + id);
-                }
-            } else {
-                System.out.println("No roes affected!");
-            }
-        }
-        catch (SQLException | ParseException e) {
-            throw new DbExcepition(e.getMessage());
-        }
-        finally {
-            DB.closeStatement(st);
-            DB.closeConnection();
-        }
+           System.out.println("Done! Rows affected: " + rowsAffected);
+
+       } catch (SQLException e) {
+           throw new DbExcepition(e.getMessage());
+       } finally {
+           DB.closeStatement(st);
+           DB.closeConnection();
+       }
 
     }
 
