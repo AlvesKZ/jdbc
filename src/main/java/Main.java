84eb8b117e7c9b799558d207a75e8c2bcab8818e
AlvesKZ <igoralvex01@gmail.com>
2026-02-04 19:24:50 -0300

    feat: get data from database

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,37 @@
 import db.DB;
+import db.DbExcepition;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        Connection conn = DB.getConnection();
-        DB.closeConnection();
+        Connection conn = null;
+        Statement st = null;
+        ResultSet rs = null;
+
+        try {
+            conn = DB.getConnection();
+
+            st = conn.createStatement();
+
+            rs = st.executeQuery("SELECT * FROM department");
+
+            while (rs.next()) {
+                System.out.println(rs.getInt("Id") + ", " + rs.getString("Name"));
+            }
+
+        } catch (SQLException e) {
+             throw new DbExcepition(e.getMessage());
+        } finally {
+            DB.resultSet(rs);
+            DB.closeStatement(st);
+            DB.closeConnection();
+        }
 
     }
 
--- src/main/java/db/DB.java
+++ src/main/java/db/DB.java
@@ -2,9 +2,7 @@ package db;
 
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.Properties;
 
 public class DB {
@@ -43,4 +41,25 @@ public class DB {
              throw new DbExcepition(e.getMessage());
          }
     }
+
+    public static void closeStatement(Statement st) {
+        if (st != null) {
+            try {
+                st.close();
+            } catch (SQLException e) {
+                throw new DbExcepition(e.getMessage());
+            }
+        }
+    }
+
+    public static void resultSet(ResultSet rs) {
+        if (st != null) {
+            try {
+                rs.close();
+            } catch (SQLException e) {
+                throw new DbExcepition(e.getMessage());
+            }
+        }
+    }
+
 }
