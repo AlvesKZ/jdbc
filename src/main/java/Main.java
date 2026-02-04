3fec6a5ba9b313caf2fdacbcd1b2defdf5b2a546
AlvesKZ <igoralvex01@gmail.com>
2026-02-04 19:56:37 -0300

    feat: insert data in database #2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -20,7 +20,8 @@ public class Main {
                     "INSERT INTO seller "
                     + "(Name, Email, BirthDate, BaseSalary, DepartmentId)"
                     + "VALUES "
-                    + "(?, ?, ?, ?, ?)"
+                    + "(?, ?, ?, ?, ?)",
+                    Statement.RETURN_GENERATED_KEYS
             );
 
             st.setString(1, "Carl");
@@ -31,8 +32,15 @@ public class Main {
 
             int rowsAffected = st.executeUpdate();
 
-            System.out.println("Done! rows affected: " + rowsAffected);
-
+            if (rowsAffected > 0) {
+                ResultSet rs = st.getGeneratedKeys();
+                while (rs.next()) {
+                    int id = rs.getInt(1);
+                    System.out.println("Done! id = " + id);
+                }
+            } else {
+                System.out.println("No roes affected!");
+            }
         }
         catch (SQLException | ParseException e) {
             throw new DbExcepition(e.getMessage());
